d8856e67084710497ac903a93162629bba657cad
Peter Grunde <peter.a.grunde@gmail.com>
2014-07-06 21:48:24 -0600

    second time through

--- app.rb
+++ app.rb
@@ -13,6 +13,40 @@ class App < Sinatra::Application
   end
 
   get "/" do
-    "Hello, world"
+    if session[:user_id]
+      @username = @user_database.find(session[:user_id])
+      @username = @username[:username]
+    end
+    erb :root, :locals => { :username => @username }, :layout => :main_layout
   end
+
+  get "/register/" do
+    erb :register, :locals => {  }, :layout => :main_layout
+  end
+
+  post "/register/" do
+    params_sym = params.inject({}) {|memo, (k,v)| memo[k.to_sym] = v; memo }
+    @user_database.insert(params_sym)
+    flash[:register_notice] = "Thank you for registering."
+    redirect "/"
+  end
+
+  post "/login/" do
+    params_sym = params.inject({}) {|memo, (k,v)| memo[k.to_sym] = v; memo }
+    logged_user = @user_database.all.find do |hash|
+      params_sym[:username] == hash[:username] && params_sym[:password] == hash[:password]
+    end
+
+    if logged_user
+      session[:user_id] = logged_user[:id]
+    end
+
+    redirect "/"
+  end
+
+  get "/logout/" do
+    session[:user_id] = nil
+    redirect "/"
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,59 @@ require "capybara/rspec"
 require "./app"
 
 Capybara.app = App
+
+feature "Homepage" do
+  scenario "Visit homepage" do
+    visit "/"
+
+    expect(page).to have_content("Register")
+  end
+  scenario "View register page" do
+    visit "/"
+
+    click_link "Register"
+
+    expect(page).to have_content("Username")
+  end
+  scenario "Create a log in" do
+    visit "/register/"
+
+    fill_in("username", :with => "pgrunde")
+    fill_in("password", :with => "password")
+
+    click_button "Sign Up"
+
+    expect(page).to have_content("Thank you for registering.")
+  end
+  scenario "Create log in and log in" do
+    visit "/register/"
+
+    fill_in("username", :with => "pgrunde")
+    fill_in("password", :with => "password")
+
+    click_button "Sign Up"
+
+    fill_in("username", :with => "pgrunde")
+    fill_in("password", :with => "password")
+
+    click_button "Log In"
+
+    expect(page).to have_content("Logout")
+  end
+  scenario "create log in and then log out" do
+    visit "/register/"
+
+    fill_in("username", :with => "pgrunde")
+    fill_in("password", :with => "password")
+
+    click_button "Sign Up"
+
+    fill_in("username", :with => "pgrunde")
+    fill_in("password", :with => "password")
+
+    click_button "Log In"
+    click_link "Logout"
+
+    expect(page).to have_content("Register")
+  end
+end
